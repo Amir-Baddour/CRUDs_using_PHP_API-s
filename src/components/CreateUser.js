59eb2e7c3d8eb34240ee7e525e4f6b0d0fcec511
Amir-Baddour <amirbaddour@users.noreply.github.com>
2025-04-16 16:19:01 +0300

    intall axios, create API folder(for backend files)

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,21 +1,22 @@
 import { useState } from "react";
+import axios from"axios";
 
 export default function CreateUser() {
-
-  const [inputs,setInputs]=useState({})
+  const [inputs, setInputs] = useState({});
 
   // function to handle form submission
-  const handleSubmit =(event)=>{
-   event.preventDefault();
-   console.log(inputs);
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post("http://localhost:8080/api/user/save", inputs);
+    console.log(inputs);
+  };
 
   // function to handle input changes
-  const handleChange =(event)=>{
-    const name=event.target.name;
-    const value=event.target.value;
-    setInputs(values => ({...values,[name]:value}));
-  }
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
   return (
     <div>
@@ -29,7 +30,12 @@ export default function CreateUser() {
                 <label>Name:</label>
               </th>
               <td>
-                <input type="text" name="name" onChange={handleChange} required />
+                <input
+                  type="text"
+                  name="name"
+                  onChange={handleChange}
+                  required
+                />
               </td>
             </tr>
             <tr>
@@ -37,7 +43,12 @@ export default function CreateUser() {
                 <label>Email:</label>
               </th>
               <td>
-                <input type="text" name="email" onChange={handleChange} required />
+                <input
+                  type="text"
+                  name="email"
+                  onChange={handleChange}
+                  required
+                />
               </td>
             </tr>
             <tr>
@@ -45,11 +56,16 @@ export default function CreateUser() {
                 <label>Mobile:</label>
               </th>
               <td>
-                <input type="text" name="mobile" onChange={handleChange} required />
+                <input
+                  type="text"
+                  name="mobile"
+                  onChange={handleChange}
+                  required
+                />
               </td>
             </tr>
             <tr>
-              <td colSpan ="2" align ="right">
+              <td colSpan="2" align="right">
                 <button type="submit">Create</button>
               </td>
             </tr>
