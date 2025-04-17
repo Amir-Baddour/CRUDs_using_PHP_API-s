14b6f7ff575af6c4095c9a86929493c95d3803c1
Amir-Baddour <amirbaddour@users.noreply.github.com>
2025-04-17 12:20:53 +0300

    create getUsr function and import the hooks

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,7 +1,29 @@
-export default  function EditUser() {
-    return (
-      <div>
-        <h1>Edit User</h1>
-      </div>
-    );
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+export default function EditUser() {
+  const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    getUser();
+  }, []);
+  useEffect(() => {
+    getUser();
+  }, []);
+  //getUser
+  function getUser() {
+    axios.get(`http://localhost:8888/api/user/${id}`).then(function (response) {
+      console.log(response.data);
+      setInputs(response.data);
+    });
   }
+  return (
+    <div>
+      <h1>Edit User</h1>
+    </div>
+  );
+}
