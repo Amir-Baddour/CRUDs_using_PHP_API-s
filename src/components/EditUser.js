143ae61344b5b895d6e3eb1334f64ad014562d4c
Amir-Baddour <amirbaddour@users.noreply.github.com>
2025-04-17 12:23:16 +0300

    create the form of the sumbmit

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -27,9 +27,54 @@ export default function EditUser() {
     const value = event.target.value;
     setInputs(values => ({...values, [name]: value}));
 }
+//handleSubmit
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  axios.put(`http://localhost:8888/api/user/${id}/edit`, inputs).then(function(response){
+      console.log(response.data);
+      navigate('/');
+  });
+  
+}
   return (
     <div>
-      <h1>Edit User</h1>
-    </div>
+            <h1>Edit user</h1>
+            <form onSubmit={handleSubmit}>
+                <table cellSpacing="10">
+                    <tbody>
+                        <tr>
+                            <th>
+                                <label>Name: </label>
+                            </th>
+                            <td>
+                                <input value={inputs.name} type="text" name="name" onChange={handleChange} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <label>Email: </label>
+                            </th>
+                            <td> 
+                                <input value={inputs.email} type="text" name="email" onChange={handleChange} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <label>Mobile: </label>
+                            </th>
+                            <td>
+                                <input value={inputs.mobile} type="text" name="mobile" onChange={handleChange} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2" align ="right">
+                                <button>Save</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
   );
 }
